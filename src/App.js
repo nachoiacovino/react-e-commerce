f431cabf6bac33fcc736edd1aa27db620e487788
Ignacio Iacovino <nacho.iacovino@gmail.com>
2019-12-01 23:11:00 +0100

    Add Navbar component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar'
 import Homepage from './pages/Homepage/Homepage'
 import Shop from './pages/Shop/Shop'
 import './App.css'
 
 const App = _ => {
   return (
-    <Switch>
-      <Route exact path="/" component={Homepage} />
-      <Route exact path="/shop" component={Shop} />
-    </Switch>
+    <div>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop" component={Shop} />
+      </Switch>
+    </div>
   )
 }
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { ReactComponent as Logo } from '../../assets/crown.svg'
+import './Navbar.scss'
+
+export default function Navbar() {
+    return (
+        <div className="Navbar">
+            <Link to="/" className="logo-container">
+                <Logo className="logo" />
+            </Link>
+            <nav className="Navbar-nav">
+                <Link to="/shop" className="Navbar-nav-item">Shop</Link>
+                <Link to="/shop" className="Navbar-nav-item">Contact</Link>
+            </nav>
+        </div>
+    )
+}
